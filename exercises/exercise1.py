6833b9bb203d1db6271d17e1589f53edea88d879
Elena Lukyanchuk <e.lukyanchukk@gmail.com>
2024-03-13 11:00:12 +0100

    Trojkat

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -3,8 +3,14 @@ bok_a = 10
 pole_kw = bok_a * bok_a
 print("Pole kwadratu wynosi: ", str(pole_kw))
 
-#Prostokąt
+# Prostokąt
 bok_a = 10
 bok_b = 7
 pole_pr = bok_a*bok_b
 print("Pole prostokątu wynosi: ", str(pole_pr))
+
+# Trójkąt
+a = 10
+h = 5
+pole_tr = 1/2*a*h
+print("Pole trójkątu wynosi: ", str(pole_tr))
