57437be78d5e8b2d0bb68b080057fe9f16ec83b9
Elena Lukyanchuk <e.lukyanchukk@gmail.com>
2024-03-13 10:57:49 +0100

    Prostokąt

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -2,3 +2,9 @@
 bok_a = 10
 pole_kw = bok_a * bok_a
 print("Pole kwadratu wynosi: ", str(pole_kw))
+
+#Prostokąt
+bok_a = 10
+bok_b = 7
+pole_pr = bok_a*bok_b
+print("Pole prostokątu wynosi: ", str(pole_pr))
